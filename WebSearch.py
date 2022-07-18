077595b622527f2dd537e75eac104ed590ddddc6
Kinoko_2K <65443692+kinoko2k@users.noreply.github.com>
2022-07-18 17:12:07 +0900

    Update WebSearch.py

--- WebSearch.py
+++ WebSearch.py
@@ -1,12 +1,22 @@
-import time
+import schedule
+from time import sleep
 from selenium import webdriver
 import chromedriver_binary
- 
-driver = webdriver.Chrome()
-driver.get('https://www.google.com/')
- 
-search = driver.find_element_by_name('q')
-search.send_keys('きのこ')
-search.submit()
-time.sleep(5)
-driver.quit()
+
+def task():
+    driver = webdriver.Chrome()
+# 指定サイトで実行
+    driver.get('https://www.google.com/')
+# 検索tagがq
+    search = driver.find_element_by_name('q')
+# 検索ワード
+    search.send_keys('リンゴ')
+    search.submit()
+    driver.quit()
+    print("Task running now")
+
+schedule.every(1800).seconds.do(task)
+
+while True:
+    schedule.run_pending()
+    sleep(1)
